2b2a55f2e06df9299d271a96193b98d30f51b7f6
Daira Hopwood <daira@jacaranda.org>
2015-04-17 21:40:51 +0100

    Add oauth2client to requirements.
    Signed-off-by: Daira Hopwood <daira@jacaranda.org>

--- src/allmydata/_auto_deps.py
+++ src/allmydata/_auto_deps.py
@@ -54,6 +54,7 @@ install_requires = [
 
     # needed for cloud backend
     "txAWS == 0.2.1.post5",
+    "oauth2client == 1.1.0",
 
     # needed for cloud backend; also latest cryptography depends on this version
     "six >= 1.4.1",
@@ -81,6 +82,7 @@ package_imports = [
     ('pyasn1-modules',   'pyasn1_modules'),
     ('mock',             'mock'),
     ('txAWS',            'txaws'),
+    ('oauth2client',     'oauth2client'),
     ('python-dateutil',  'dateutil'),
     ('six',              'six'),
 ]
